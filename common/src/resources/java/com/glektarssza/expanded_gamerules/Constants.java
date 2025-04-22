23d8a18cc848553ecf8c2db097507784368abef5
G'lek Tarssza <glek@glektarssza.com>
2025-04-22 01:42:57 -0600

    Failing build for bug reference.

---
+++ common/src/resources/java/com/glektarssza/expanded_gamerules/Constants.java
@@ -0,0 +1,10 @@
+package com.glektarssza.expanded_gamerules;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "expanded_gamerules";
+	public static final String MOD_NAME = "Expanded Gamerules";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
